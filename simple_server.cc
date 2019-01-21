841e0d9696ccabab1e97afb7bd063836a4886d0c
Oliver <test@example.com>
2019-01-21 02:03:59 +0000

    Running server

--- simple_server.cc
+++ simple_server.cc
@@ -1,15 +1,48 @@
+#include <iostream>
+#include <memory>
+#include <string>
+
 #include <grpc/grpc.h>
 #include <grpcpp/server.h>
 #include <grpcpp/server_builder.h>
 #include <grpcpp/server_context.h>
+#include <grpcpp/security/server_credentials.h>
 #include "messagedef.grpc.pb.h"
 
-class SimpleServerImpl  final : public SampleData::Service {
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+using messagedef::SampleDataMessage;
+using messagedef::Response;
+using messagedef::SampleData;
+
+
+class SimpleServerImpl final : public SampleData::Service {
+
+    Status SendData(ServerContext* context, const SampleDataMessage* message, Response* response) override
+    {
+        response->set_message("test");
+        return Status::OK;
+    }
 
 };
 
-int main()
+void RunServer() 
 {
-    //XXX: Temporary
+    std::string server_address("0.0.0.0:50051");
+    SimpleServerImpl service;
+
+    ServerBuilder builder;
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    builder.RegisterService(&service);
+    std::unique_ptr<Server> server(builder.BuildAndStart());
+    std::cout << "Server listening on " << server_address << std::endl;
+    server->Wait();
+}
+
+int main(int argc, char** argv)
+{
+    RunServer();
     return 0;
 }
